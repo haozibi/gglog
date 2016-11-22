fa4e542cca6fbec979963fffa4f030974c91c839
haozibi <haozibi@foxmail.com>
2016-11-22 21:07:56 +0800

    go fmt

--- gglog_file.go
+++ gglog_file.go
@@ -40,8 +40,8 @@ var logDirs []string
 // See createLogDirs for the full list of possible destinations.
 var logDir = flag.String("log_dir", "", "If non-empty, write log files in this directory")
 
-func SetLogDir(dir string){
-    logDirs = append(logDirs, dir)
+func SetLogDir(dir string) {
+	logDirs = append(logDirs, dir)
 }
 
 func createLogDirs() {
